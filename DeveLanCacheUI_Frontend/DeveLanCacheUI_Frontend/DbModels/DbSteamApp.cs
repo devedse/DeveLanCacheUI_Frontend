f758aa6aba48193ff51cce2927177f9d1d1d4a8e
Devedse <davydavidse@gmail.com>
2023-07-01 02:55:04 +0200

    Add frontend

---
+++ DeveLanCacheUI_Frontend/DeveLanCacheUI_Frontend/DbModels/DbSteamApp.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeveLanCacheUI_Frontend.DbModels
+{
+    public class DbSteamApp
+    {
+        [Key]
+        public int Id { get; set; }
+        public string AppName { get; set; }
+    }
+}
---
+++ DeveLanCacheUI_Frontend/DeveLanCacheUI_Frontend/DbModels/DbSteamAppDownloadEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeveLanCacheUI_Frontend.DbModels
+{
+    public class DbSteamAppDownloadEvent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int SteamDepotId { get; set; }
+        public DbSteamDepot SteamDepot { get; set; } = null!;
+
+        public required string ClientIp { get; set; }
+
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime LastUpdatedAt { get; set; }
+
+        public long CacheHitBytes { get; set; }
+        public long CacheMissBytes { get; set; }
+    }
+}
