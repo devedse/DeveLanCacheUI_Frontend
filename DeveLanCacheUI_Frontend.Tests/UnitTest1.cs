bd2ad904a8a3cc1e0f96c80fba8674eb7a32d112
Devedse <davydavidse@gmail.com>
2023-07-03 19:08:49 +0200

    Added test rpoject

---
+++ DeveLanCacheUI_Frontend.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DeveLanCacheUI_Frontend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
