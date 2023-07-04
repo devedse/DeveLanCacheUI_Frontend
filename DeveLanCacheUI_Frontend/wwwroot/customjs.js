0fe969eeb2b7ee3567dc19e76053f90252c612c8
Devedse <davydavidse@gmail.com>
2023-07-05 00:33:54 +0200

    Add dark theme

--- DeveLanCacheUI_Frontend/wwwroot/customjs.js
+++ DeveLanCacheUI_Frontend/wwwroot/customjs.js
@@ -2,3 +2,12 @@ window.scrollChatToBottom = () => {
     var chatBox = document.querySelector('.chat-box');
     chatBox.scrollTop = chatBox.scrollHeight;
 }
+
+window.themeSwitcher = {
+    switchTheme: function (theme) {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    },
+    getPreferredColorScheme: function () {
+        return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+    }
+};
