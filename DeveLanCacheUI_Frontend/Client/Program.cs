502aa14938d339eb33b6b06d709e2dd754cd8e3e
Devedse <davydavidse@gmail.com>
2023-07-16 14:50:33 +0200

    Added robots everywhere +local storage

--- DeveLanCacheUI_Frontend/Client/Program.cs
+++ DeveLanCacheUI_Frontend/Client/Program.cs
@@ -1,8 +1,11 @@
+using Blazored.LocalStorage;
 using DeveLanCacheUI_Frontend;
 using DeveLanCacheUI_Frontend.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DeveLanCacheUI_Frontend
 {
@@ -16,6 +19,8 @@ namespace DeveLanCacheUI_Frontend
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<BackendConnectionService>();
+
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
---
+++ DeveLanCacheUI_Frontend/Services/LocalStorageField.cs
@@ -0,0 +1,8 @@
+﻿namespace DeveLanCacheUI_Frontend.Services
+{
+    public static class LocalStorageField
+    {
+        public static string DarkModeEnabled = nameof(DarkModeEnabled);
+        public static string HashImagesEnabled = nameof(HashImagesEnabled);
+    }
+}
